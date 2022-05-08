4ab25097278c110d69154d75ae5e5f77d42311d9
VitaliiKotsyubinsky <magnoom2010@gmail.com>
2022-05-08 22:41:34 +0300

    add input and remove btn

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,33 @@
   <div id="app">
     <h1>Todo Application</h1>
     <hr>
-
+    <TodoList v-bind:todos="todos" @remove-todo="removeTodo" />
   </div>
 </template>
 
 <script>
 
-
+import TodoList from "@/components/TodoList"
 export default {
-  name: 'App',
-  components: {
+  name: 'app',
+  data() {
+    return {
+      todos: [
+        { id: 1, title: 'Купить что-то', completed: false },
+        { id: 2, title: 'Купить что-то', completed: false },
+        { id: 3, title: 'Купить что-то', completed: false }
+      ]
+    }
 
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    }
+
+  },
+  components: {
+    TodoList
   }
 }
 </script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,56 @@
+<template>
+    <li>
+        <span v-bind:class="{ done: todo.completed }">
+            <input type="checkbox" v-on:change="todo.completed = !todo.completed">
+            <strong>{{ todo.id }}</strong>
+            {{ todo.title }}
+
+        </span>
+        <button class="rm" v-on:click="$emit('remove-todo', todo.id)">&times;</button>
+    </li>
+
+
+</template>
+
+<script>
+import { on } from "events";
+
+export default {
+    props: {
+        todo: {
+            type: Object,
+            require: true
+
+        }
+    }
+}
+</script>
+<style>
+li {
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: space-between;
+    padding: .5rem 2rem;
+    margin-bottom: 1rem;
+    text-align: center;
+}
+
+.rm {
+    border: none;
+    background-color: red;
+    color: #fff;
+    border-radius: 50%;
+    font-weight: bold;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+}
+
+input {
+    margin-right: 1rem;
+}
+
+.done {
+    text-decoration: line-through;
+}
+</style>
